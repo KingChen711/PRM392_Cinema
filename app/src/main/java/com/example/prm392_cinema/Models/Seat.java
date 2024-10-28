768172605c6ee5f93f559e3d11d69ec5b0461205
Trần Trương Văn <kingchenobama711@gmail.com>
2024-10-28 16:15:35 +0700

    call seats

--- app/src/main/java/com/example/prm392_cinema/Models/Seat.java
+++ app/src/main/java/com/example/prm392_cinema/Models/Seat.java
@@ -1,28 +1,102 @@
 package com.example.prm392_cinema.Models;
 
+import java.util.Objects;
+
 public class Seat {
     public static final int STATUS_AVAILABLE = 0;
     public static final int STATUS_BOOKED = 1;
     public static final int STATUS_SELECTED = 2;
 
-    private final boolean isEmpty;
+    public static final String TYPE_NORMAL = "Ghế Thường";
+    public static final String TYPE_VIP = "Ghế VIP";
+
+    //from model
+    private int seatId;
+    private String seatType;
+    private int price;
+    private boolean isSeat;
+    private String name;
+    private boolean isSold;
+    private int colIndex;
+    private int seatIndex;
+
+    //from client
     private int status;  //checked, ordered, ready
-    private int index;  //checked, ordered, ready
 
-    public Seat(boolean isEmpty) {
-        this.isEmpty = isEmpty;
-        this.status = STATUS_AVAILABLE;
-        this.index = -1;
+    public Seat(int seatId, String seatType, int price, boolean isSeat, String name, boolean isSold, int colIndex, int seatIndex) {
+        this.seatId = seatId;
+        this.seatType = seatType;
+        this.price = price;
+        this.isSeat = isSeat;
+        this.name = name;
+        this.isSold = isSold;
+        this.colIndex = colIndex;
+        this.seatIndex = seatIndex;
+        this.status = isSold ? STATUS_BOOKED : STATUS_AVAILABLE;
     }
 
-    public Seat(int status, int index) {
-        this.isEmpty = false;
-        this.status = status;
-        this.index = index;
+    public int getSeatId() {
+        return seatId;
     }
 
-    public boolean isEmpty() {
-        return isEmpty;
+    public void setSeatId(int seatId) {
+        this.seatId = seatId;
+    }
+
+    public String getSeatType() {
+        return seatType;
+    }
+
+    public void setSeatType(String seatType) {
+        this.seatType = seatType;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public boolean isSeat() {
+        return isSeat;
+    }
+
+    public void setSeat(boolean seat) {
+        isSeat = seat;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public boolean isSold() {
+        return isSold;
+    }
+
+    public void setSold(boolean sold) {
+        isSold = sold;
+    }
+
+    public int getColIndex() {
+        return colIndex;
+    }
+
+    public void setColIndex(int colIndex) {
+        this.colIndex = colIndex;
+    }
+
+    public int getSeatIndex() {
+        return seatIndex;
+    }
+
+    public void setSeatIndex(int seatIndex) {
+        this.seatIndex = seatIndex;
     }
 
     public int getStatus() {
@@ -31,14 +105,6 @@ public class Seat {
 
     public void setStatus(int status) {
         this.status = status;
-    }
-
-    public int getIndex() {
-        return index;
-    }
-
-    public void setIndex(int index) {
-        this.index = index;
     }
 
     public boolean isBooked() {
@@ -51,5 +117,13 @@ public class Seat {
 
     public boolean isSelected() {
         return status == STATUS_SELECTED;
+    }
+
+    public boolean isNormal() {
+        return Objects.equals(seatType, TYPE_NORMAL);
+    }
+
+    public boolean isVip() {
+        return Objects.equals(seatType, TYPE_VIP);
     }
 }
---
+++ app/src/main/java/com/example/prm392_cinema/Services/ApiClient.java
@@ -0,0 +1,29 @@
+package com.example.prm392_cinema.Services;
+
+import java.security.cert.CertificateException;
+
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSocketFactory;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
+
+import okhttp3.OkHttpClient;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class ApiClient {
+    private static final String BASE_URL = "https://prmcinestar.ddnsking.com";
+    private static Retrofit retrofit;
+
+    public static Retrofit getRetrofitInstance() {
+        if (retrofit == null) {
+            retrofit = new Retrofit.Builder()
+                    .baseUrl(BASE_URL)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
+        }
+        return retrofit;
+    }
+
+}
+
